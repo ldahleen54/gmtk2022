1c7fdc51155f601e1285aa424e943210aa0c04b5
ldahleen54 <ldahleen54@gmail.com>
2022-07-16 00:10:51 -0500

    Code cleanup, added title screen and title screen selector

--- PlayerModel.gd
+++ PlayerModel.gd
@@ -3,10 +3,7 @@ extends Node
 var items = ["dice necklace"]
 # not sure if this should be declared here or elsewhere and if this will be reset after every scene
 var health: int = 6
-var current_scene = null
 
 func _process(delta):
-	print("health: ", health)
 	if health <= 0:
-		print("You died")
 		get_tree().change_scene("res://scenes/death.tscn")
---
+++ Selector.gd
@@ -0,0 +1,22 @@
+extends Sprite
+
+var selected_index = 0
+var scenes = [
+	"res://scenes/area1.tscn",
+	"res://scenes/credits.tscn"
+]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_offset(Vector2(0, -297))
+	print("test")
+	
+func _process(delta):
+	if Input.is_action_pressed("ui_down") and selected_index < len(scenes) - 1:
+		selected_index = selected_index + 1
+		set_offset(Vector2(0, 0))
+	if Input.is_action_pressed("ui_up") and selected_index > 0:
+		selected_index = selected_index - 1
+		set_offset(Vector2(0, -297))
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene(scenes[selected_index])
+			
