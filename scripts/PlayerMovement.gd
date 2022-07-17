97ae28175128c529122a07303878c2a846259a95
InsaneXArtist <72520687+InsaneXArtist@users.noreply.github.com>
2022-07-17 13:08:56 -0400

    Fixed danger7.

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -212,3 +212,7 @@ func _on_Randomteleporter3_body_entered(body):
 
 func _on_RandomTeleport_body_entered(body):
 	pass # Replace with function body.
+
+
+func _on_Randomteleporter2_body_entered(body):
+	pass # Replace with function body.
