2e9243fb811262117cc24a8592e83127401e6013
InsaneXArtist <72520687+InsaneXArtist@users.noreply.github.com>
2022-07-17 12:41:10 -0400

    Added working player and enemy to danger9 and danger10, fixed portals for those areas.

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -202,3 +202,7 @@ func _on_Secret1Teleporter_body_entered(body):
 
 func _on_Randomteleporter3_body_entered(body):
 	pass # Replace with function body.
+
+
+func _on_RandomTeleport_body_entered(body):
+	pass # Replace with function body.
