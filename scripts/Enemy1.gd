cb8fadd29fb2c7aa70bf2b12de61ca769aae1803
ldahleen54 <ldahleen54@gmail.com>
2022-07-17 10:50:07 -0500

    Fixed bug where enemy stays on top of player

--- scripts/Enemy1.gd
+++ scripts/Enemy1.gd
@@ -3,7 +3,17 @@ extends KinematicBody2D
 var speed = 300
 var move = Vector2.ZERO
 onready var player_model = get_node("/root/PlayerModel")
+onready var previous_player_health = player_model.health
+onready var velocity = Vector2.ZERO
+var cooldown = 0
 
 func _physics_process(delta):
-	var velocity = position.direction_to(player_model.position) * speed
+	if cooldown > 0:
+		cooldown = cooldown - 1
+		return null
+	if player_model.health < previous_player_health:
+		cooldown = 20
+		velocity = Vector2.ZERO
+		previous_player_health = player_model.health
+	velocity = position.direction_to(player_model.position) * speed
 	move_and_slide(velocity)
